f2512b45715b299cab1c6b80d89eade7defaa93a
hwangyoungjin <yoho555@icloud.com>
2021-12-16 12:31:49 +0900

    feat: v2 init

---
+++ v2/src/main/kotlin/com/example/springelasticsearchv2/Springelasticsearchv2Application.kt
@@ -0,0 +1,11 @@
+package com.example.springelasticsearchv2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Springelasticsearchv2Application
+
+fun main(args: Array<String>) {
+    runApplication<Springelasticsearchv2Application>(*args)
+}
---
+++ v2/src/test/kotlin/com/example/springelasticsearchv2/Springelasticsearchv2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springelasticsearchv2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Springelasticsearchv2ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
