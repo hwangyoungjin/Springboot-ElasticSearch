5c967724455e073788fcc5b8179fd4cb5c7fe08a
hwangyoungjin <yoho555@icloud.com>
2021-12-16 11:59:06 +0900

    feat: init v2

--- src/main/kotlin/com/example/springelasticsearch/repository/CustomUserSearchRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.springelasticsearch.repository
-
-import com.example.springelasticsearch.entity.User
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.Pageable
-
-interface CustomUserSearchRepository {
-    fun searchByName(name : String, pageable: Pageable) : List<User>
-}
--- src/main/kotlin/com/example/springelasticsearch/repository/UserSearchRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.springelasticsearch.repository
-
-import com.example.springelasticsearch.entity.User
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
-
-interface UserSearchRepository : ElasticsearchRepository<User,Long> , CustomUserSearchRepository {
-
-    fun findByBasicProfile_NameContains(name: String) : List<User>
-}
